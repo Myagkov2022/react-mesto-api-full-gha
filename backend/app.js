8ca757f1841f8d039b2f4cc9226b05122db45537
Ivan Myagkov <myagkovivan2022@yandex.ru>
2023-08-12 22:48:59 +0300

    first commit

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,11 @@ const { errors } = require('celebrate');
 const router = require('./routes/index');
 const errorsHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors')
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cors)
+app.use(cors);
 app.use(requestLogger);
 app.use(express.json());
 app.use(router);
